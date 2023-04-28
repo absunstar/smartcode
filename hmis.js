db2db48f1ff197e14c7904cbd5a25fb1fd26f4e6
baher hamed <baherhamed@gmail.com>
2023-04-28 20:43:09 +0300

    add tab reports

--- hmis.js
+++ hmis.js
@@ -1,51 +1,53 @@
 const site = require('../isite')({
-  port: [12345],
-  lang: 'En',
-  version: Date.now(),
-  name: 'HMIS',
-  require: {
-    features: [],
-    permissions: [],
-  },
-  theme: 'theme_paper',
-  mongodb: {
-    db: 'HMIS',
-    limit: 1000,
-    identity: {
-      enabled: !0,
+    port: [12345],
+    lang: 'En',
+    version: Date.now(),
+    name: 'HMIS',
+    require: {
+        features: [],
+        permissions: [],
     },
-  },
-  security: {
-    keys: ['21232f297a57a5a743894a0e4a801fc3', 'f6fdffe48c908deb0f4c3bd36c032e72'],
-  },
+    theme: 'theme_paper',
+    mongodb: {
+        db: 'HMIS',
+        limit: 1000,
+        identity: {
+            enabled: !0,
+        },
+    },
+    security: {
+        keys: ['21232f297a57a5a743894a0e4a801fc3', 'f6fdffe48c908deb0f4c3bd36c032e72'],
+    },
 });
 
 site.get({
-  name: '/',
-  path: site.dir + '/',
+    name: '/',
+    path: site.dir + '/',
 });
 
 site.get(
-  {
-    name: '/',
-  },
-  (req, res) => {
-    res.render('index.html', { title: req.word('Site Title'),appName : 'Home Page' }, { parser: 'html', compres: true });
-  }
+    {
+        name: '/',
+    },
+    (req, res) => {
+        res.render('index.html', { title: req.word('Site Title'), appName: 'Home Page' }, { parser: 'html', compres: true });
+    }
 );
 
 site.importApps(__dirname + '/appsAccounting');
 site.importApps(__dirname + '/appsInventories');
+site.importApps(__dirname + '/reports');
 site.importApps(__dirname + '/appsHr');
 site.importApps(__dirname + '/appsHmis');
-site.importApp(__dirname + '/ui-print')
+site.importApp(__dirname + '/ui-print');
 
 site.loadLocalApp('client-side');
 site.loadLocalApp('ui-print');
 
 site.addFeature('m-hmis');
-site.addFeature('m-inventory')
-site.addFeature('m-accounting')
-site.addFeature('m-hr')
+site.addFeature('m-inventory');
+site.addFeature('m-accounting');
+site.addFeature('m-hr');
+site.addFeature('m-reports');
 
 site.run();
