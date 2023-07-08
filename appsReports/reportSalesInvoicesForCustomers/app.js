d20290633e915e08f9a2bb305a91e57ae0fc8f64
baher hamed <baherhamed@gmail.com>
2023-07-08 23:40:03 +0200

    add reportSalesInvoicesForCustomer vand reportSalesInvoicesForCustomers

---
+++ appsReports/reportSalesInvoicesForCustomers/app.js
@@ -0,0 +1,22 @@
+module.exports = function init(site) {
+  let app = {
+    name: 'reportSalesInvoicesForCustomers',
+    allowRoute: true,
+    allowRouteGet: true,
+  };
+
+  app.$collection = site.connectCollection(app.name);
+
+  if (app.allowRoute) {
+    if (app.allowRouteGet) {
+      site.get(
+        {
+          name: app.name,
+        },
+        (req, res) => {
+          res.render(app.name + '/index.html', { title: app.name, appName: 'Report Sales Invoices For Customers' }, { parser: 'html', compres: true });
+        }
+      );
+    }
+  }
+};
